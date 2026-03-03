58b221adbef0db3219c312f1ffd23bc0825a2fcd
hassehulabeck <hans.2.andersson@educ.goteborg.se>
2026-03-03 20:33:11 +0100

    Added two classes by CSS modules

--- src/StudentList.jsx
+++ src/StudentList.jsx
@@ -5,6 +5,7 @@ export function StudentList({children}) {
         <div className="studentList">
             <h1 className={styles.h1}>Studentlist</h1>
             <div className="border-8">
+                <p className={ [styles.underl, styles.fat].join(" ") }>Hello</p>
                 { children }
             </div>
         </div>
