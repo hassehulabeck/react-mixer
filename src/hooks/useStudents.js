a2be97efef0d83676d986433762ed90dc341386a
hassehulabeck <hans.2.andersson@educ.goteborg.se>
2026-03-29 12:25:33 +0200

    edited fetch url

--- src/hooks/useStudents.js
+++ src/hooks/useStudents.js
@@ -17,7 +17,7 @@ export function useStudents() {
     }, [students])
 
     const fetchData = () => {
-    fetch('./public/students.json')
+    fetch('/students.json')
         .then ((response) => response.json())
         .then ((json) => {
         setStudents(json.students)
