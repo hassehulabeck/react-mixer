f4d6036765b77a4d8e03dab9be5ed09cb5604953
hassehulabeck <hans.2.andersson@educ.goteborg.se>
2026-03-29 12:18:02 +0200

    moved data folder

--- src/hooks/useStudents.js
+++ src/hooks/useStudents.js
@@ -17,7 +17,7 @@ export function useStudents() {
     }, [students])
 
     const fetchData = () => {
-    fetch('./src/data/students.json')
+    fetch('./public/data/students.json')
         .then ((response) => response.json())
         .then ((json) => {
         setStudents(json.students)
